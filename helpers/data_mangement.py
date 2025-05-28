c57ef5b27a4418cb68be85f1a6f4f6dc20becd1f
Tech08mag <tech08mag@gmail.com>
2025-05-28 23:04:16 +0200

    reformatting files

--- helpers/data_mangement.py
+++ helpers/data_mangement.py
@@ -7,12 +7,14 @@ from rich.console import Console
 
 console = Console()
 
+
 def show_all_stored_data():
-     with open('links.csv') as csv_file:
+    with open('links.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             console.print(f"[yellow]{row[0]}[/yellow], [magenta]{row[1]}[/magenta], [blue]{row[2]}[/blue]")
         csv_file.close()
+
 
 def updater():
     with open('links.csv') as csv_file:
@@ -34,6 +36,7 @@ def write_data(playlist_name: str, link_value: str, path: str):
 
         link_writer.writerow([playlist_name, link_value, path])
         link_file.close()
+
 
 def download(link: str):
     os.system(f"python3 -m spotdl {link}")
--- main.py
+++ main.py
@@ -8,9 +8,7 @@ from helpers.data_mangement import write_data, download, updater, show_all_store
 
 debug = True
 FORMAT = "%(message)s"
-logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
-)
+logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
 
 
 console = Console()
@@ -21,27 +19,24 @@ def new_entry():
     link = console.input("Add [blue]Link[/blue] to your Spotify Watcher: ")
     if link.startswith("https://open.spotify.com/"):
         name = console.input("Please enter the [green]Name[/green] for your Playlist/Album: ")
-        location =  questionary.path("Please enter the Location where you want to store your files: ").ask()
+        location = questionary.path("Please enter the Location where you want to store your files: ").ask()
         write_data(name, link, location)
     else:
         console.print("Your [red]Link[/red] have to be from Spotify!")
         return
 
-choices=["1 downlaod a single Song", 
-        "2 Add a new Album/Playlist to the Watchlist", 
-        "3 Manuelly update your Album/Playlists",
-        "4 Display all stored data",
-        ]
+
+choices = [
+    "1 downlaod a single Song",
+    "2 Add a new Album/Playlist to the Watchlist",
+    "3 Manuelly update your Album/Playlists",
+    "4 Display all stored data",
+]
+
 
 def menu():
     while True:
-        num = (
-        questionary.select(
-            "What do you want to do?",
-            choices=choices,
-        ).ask()
-        or "do nothing"
-    )
+        num = questionary.select("What do you want to do?", choices=choices).ask() or "do nothing"
         if num == choices[0]:
             link = console.input("Enter the [blue]Link[/blue]: ")
             download(link)
@@ -54,6 +49,7 @@ def menu():
         else:
             console.print("Please select an [red]availible[/red] Option!")
             return False
+
 
 if __name__ == "__main__":
     menu()
